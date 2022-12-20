73b00a4189c574b9dcb3068b0f4946485f82cec2
Loris Leiva <loris.leiva@gmail.com>
2022-12-20 20:28:33 +0000

    wip

--- packages/js-core/test/Serializer.test.ts
+++ packages/js-core/test/Serializer.test.ts
@@ -41,6 +41,26 @@ test('[Serializer] playground', (t: Test) => {
   );
   log(serializer5, { a: 123 });
 
+  type DummyStrict = { discriminator: number; label: string };
+  type DummyLoose = { discriminator?: number; label: string };
+  const serializer6: Serializer<DummyStrict> = {
+    description: 'DummyStrict',
+    serialize: (value: DummyStrict) => {
+      return new Uint8Array([value.discriminator, value.label.length]);
+    },
+    deserialize: (buffer: Uint8Array): [DummyStrict, number] => {
+      return [{ discriminator: buffer[0], label: 'x'.repeat(buffer[1]) }, 1];
+    },
+  };
+  log(serializer6, { discriminator: 5, label: 'hello world!' });
+
+  const serializer7: Serializer<DummyLoose, DummyStrict> = mapSerializer(
+    serializer6,
+    (value: DummyLoose): DummyStrict => ({ discriminator: 3, ...value })
+  );
+  log(serializer7, { discriminator: 5, label: 'hello world!' });
+  log(serializer7, { label: 'hello world!' });
+
   t.end();
 });
 
