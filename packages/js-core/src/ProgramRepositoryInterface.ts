520b2631812f2ccb8fcd0215666897ff06190b1c
Loris Leiva <loris.leiva@gmail.com>
2022-12-21 18:21:23 +0000

    wip

--- packages/js-core/src/ProgramRepositoryInterface.ts
+++ packages/js-core/src/ProgramRepositoryInterface.ts
@@ -1,8 +1,12 @@
 import type { Program } from './Program';
+import type { PublicKey } from './PublicKey';
 
 export interface ProgramRepositoryInterface {
-  get<T extends Program = Program>(name: string): T;
-  get<T extends Program = Program>(address: { toBase58: () => string }): T;
+  get<T extends Program = Program>(nameOrAddress: string | PublicKey): T;
+  getAddress(
+    nameOrAddress: string | PublicKey,
+    fallback?: PublicKey
+  ): PublicKey;
   all(): Program[];
   add(program: Program): void;
 }
