6d11ed197ffc67097ea53f09c79d1847fece428b
Loris Leiva <loris.leiva@gmail.com>
2022-12-21 13:11:32 +0000

    wip

--- packages/js-core/src/SerializerInterface.ts
+++ packages/js-core/src/SerializerInterface.ts
@@ -2,7 +2,7 @@ import type { PublicKey } from './PublicKey';
 import type { PublicKeyInput } from './EddsaInterface';
 import type { Serializer } from './Serializer';
 import type { Context } from './Context';
-import { RecordToTuple, WrapInSerializer } from './TypeUtils';
+import type { WrapInSerializer } from './TypeUtils';
 
 export type ScalarEnum<T> = Record<keyof T, number | string> & {
   [k: number]: string;
@@ -10,16 +10,26 @@ export type ScalarEnum<T> = Record<keyof T, number | string> & {
 
 export type DataEnumUnion = { __kind: string };
 
-export type DataEnumRecord<T extends { __kind: string }> = {
+export type DataEnumRecord<T extends DataEnumUnion> = {
   [P in T['__kind']]: Extract<T, { __kind: P }>;
 };
 
-export type DataEnumSerializerRecord<T extends DataEnumUnion> =
-  WrapInSerializer<DataEnumRecord<T>>;
-
-export type DataEnumSerializerTuple<T extends DataEnumUnion> = RecordToTuple<
-  DataEnumSerializerRecord<T>
+export type StructToSerializerTuple<T extends object> = Array<
+  {
+    [K in keyof T]: T[K] extends undefined ? [K] : [K, Serializer<T[K]>];
+  }[keyof T]
 >;
+
+export type DataEnumToSerializerTuple<T extends DataEnumUnion> = Array<
+  T extends any ? [T['__kind'], Serializer<Omit<T, '__kind'>>] : never
+>;
+
+// export type DataEnumSerializerRecord<T extends DataEnumUnion> =
+//   WrapInSerializer<DataEnumRecord<T>>;
+
+// export type DataEnumSerializerTuple<T extends DataEnumUnion> = RecordToTuple<
+//   DataEnumSerializerRecord<T>
+// >;
 
 export interface SerializerInterface {
   // Lists.
@@ -47,9 +57,8 @@ export interface SerializerInterface {
   ) => Serializer<T | null>;
 
   // Structs.
-  // TODO(loris): Link struct with serializer fields (remove any).
   struct: <T extends object>(
-    fields: RecordToTuple<WrapInSerializer<T>>,
+    fields: StructToSerializerTuple<T>,
     description?: string
   ) => Serializer<T>;
 
@@ -59,7 +68,7 @@ export interface SerializerInterface {
     description?: string
   ): Serializer<T>;
   dataEnum<T extends DataEnumUnion>(
-    fields: DataEnumSerializerTuple<T>,
+    fields: DataEnumToSerializerTuple<T>,
     description?: string
   ): Serializer<T>;
 
@@ -87,16 +96,15 @@ export interface SerializerInterface {
 }
 
 const foo = {} as SerializerInterface;
-const bar = foo.struct<{ a: number; b: string }>([
-  ['a', foo.u8],
-  ['b', foo.u8],
+const bar = foo.dataEnum<
+  { __kind: 'V1'; a: number } | { __kind: 'V2'; b: string }
+>([
+  ['V1', foo.struct<{ a: number }>([['a', foo.u8]])],
+  ['V2', foo.struct<{ b: string }>([['b', foo.string]])],
 ]);
-// const bar = foo.dataEnum<
-//   { __kind: 'V1'; a: number } | { __kind: 'V2'; b: string }
-// >([
-//   ['V1', foo.struct({ a: foo.u8 })],
-//   ['V1', foo.struct({ b: foo.string })],
-// ]);
 
 type T1 = { a: number; b: string };
-type T2 = RecordToTuple<WrapInSerializer<T1>>;
+type T2 = StructToSerializerTuple<T1>;
+
+type T3 = { __kind: 'V1'; a: number } | { __kind: 'V2'; b: string };
+type T4 = DataEnumToSerializerTuple<T3>;
--- packages/js-core/src/TypeUtils.ts
+++ packages/js-core/src/TypeUtils.ts
@@ -1,7 +1,5 @@
 import type { Serializer } from './Serializer';
 
-export type RecordToTuple<T extends object> = [keyof T, T[keyof T]][];
-
 export type WrapInSerializer<T> = {
   [P in keyof T]: Serializer<T[P]>;
 };
