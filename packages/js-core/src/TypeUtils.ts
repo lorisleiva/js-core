0df22e7422863d9f739b5f571b4833b79bc1cacd
Loris Leiva <loris.leiva@gmail.com>
2022-12-21 12:06:01 +0000

    wip

--- packages/js-core/src/TypeUtils.ts
+++ packages/js-core/src/TypeUtils.ts
@@ -1,10 +1,6 @@
 import type { Serializer } from './Serializer';
 
-export type Lookup<T, K> = K extends keyof T ? T[K] : never;
-
-export type RecordToTuple<T, K extends Array<keyof T> = Array<keyof T>> = {
-  [I in keyof K]: [K[I], Lookup<T, K[I]>];
-};
+export type RecordToTuple<T extends object> = [keyof T, T[keyof T]][];
 
 export type WrapInSerializer<T> = {
   [P in keyof T]: Serializer<T[P]>;
