52098d8fd123a98bce4ec1e3e634cea29e8dea02
Loris Leiva <loris.leiva@gmail.com>
2022-12-27 16:21:50 +0000

    wip

--- packages/js-serializer-beet/test/BeetSerializer.test.ts
+++ packages/js-serializer-beet/test/BeetSerializer.test.ts
@@ -2,28 +2,55 @@ import type { Serializer } from '@lorisleiva/js-core';
 import test from 'ava';
 import { BeetSerializer } from '../src';
 
-test('[js-serializer-beet] it can serialize u8 number', async (t) => {
+test('[js-serializer-beet] it can serialize u8 numbers', (t) => {
   const { u8 } = new BeetSerializer();
-  t.deepEqual(u8.serialize(0), new Uint8Array([0]));
-  t.deepEqual(u8.serialize(42), new Uint8Array([42]));
-  t.deepEqual(u8.serialize(255), new Uint8Array([255]));
-  t.is(wrapUnwrap(u8, 0), 0);
-  t.is(wrapUnwrap(u8, 42), 42);
-  t.throws<RangeError>(() => u8.serialize(-1));
-  t.throws<RangeError>(() => u8.serialize(256));
+  t.is(s(u8, 0), '00');
+  t.is(s(u8, 42), '42');
+  t.is(s(u8, 255), 'ff');
+  t.is(sd(u8, 0), 0);
+  t.is(sd(u8, 42), 42);
+  t.is(sd(u8, 255), 255);
+  t.throws<RangeError>(() => s(u8, -1));
+  t.throws<RangeError>(() => s(u8, 256));
 });
 
-test('[js-serializer-beet] it can serialize u16 number', async (t) => {
+test('[js-serializer-beet] it can serialize u16 numbers', (t) => {
   const { u16 } = new BeetSerializer();
-  t.deepEqual(u16.serialize(0), new Uint8Array([0, 0]));
-  t.deepEqual(u16.serialize(42), new Uint8Array([42, 0]));
-  t.deepEqual(u16.serialize(65535), new Uint8Array([255, 255]));
-  t.is(wrapUnwrap(u16, 0), 0);
-  t.is(wrapUnwrap(u16, 42), 42);
-  t.throws<RangeError>(() => u16.serialize(-1));
-  t.throws<RangeError>(() => u16.serialize(65536));
+  t.is(s(u16, 0), '0000');
+  t.is(s(u16, 42), '4200');
+  t.is(s(u16, 255), 'ff00');
+  t.is(s(u16, 256), 'ff01');
+  t.is(s(u16, 65535), 'ffff');
+  t.is(sd(u16, 0), 0);
+  t.is(sd(u16, 42), 42);
+  t.is(sd(u16, 65535), 65535);
+  t.throws<RangeError>(() => s(u16, -1));
+  t.throws<RangeError>(() => s(u16, 65536));
 });
 
-function wrapUnwrap<T>(serializer: Serializer<T>, value: T) {
-  return serializer.deserialize(serializer.serialize(value))[0];
+function s<T>(serializer: Serializer<T>, value: T): string {
+  return toHexString(serializer.serialize(value));
+}
+
+function d<T>(serializer: Serializer<T>, value: string): T {
+  const bytes = fromHexString(value);
+  return serializer.deserialize(bytes)[0];
+}
+
+function sd<T>(serializer: Serializer<T>, value: T) {
+  return d(serializer, s(serializer, value));
+}
+
+function fromHexString(value: string): Uint8Array {
+  const matches = value.match(/.{1,2}/g);
+  return Uint8Array.from(
+    matches ? matches.map((byte: string) => parseInt(byte, 16)) : []
+  );
+}
+
+function toHexString(bytes: Uint8Array): string {
+  return bytes.reduce(
+    (str, byte) => str + byte.toString(16).padStart(2, '0'),
+    ''
+  );
 }
