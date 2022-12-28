47153d5592f6538c3e1092994692c717a21a86ee
Loris Leiva <loris.leiva@gmail.com>
2022-12-28 01:15:29 +0000

    wip

--- packages/js-serializer-beet/test/BeetSerializer.test.ts
+++ packages/js-serializer-beet/test/BeetSerializer.test.ts
@@ -325,6 +325,48 @@ test('[js-serializer-beet] it can serialize arrays', (t) => {
   });
 });
 
+test('[js-serializer-beet] it can serialize maps', (t) => {
+  const { map, u8, string } = new BeetSerializer();
+
+  // Description matches the vec definition.
+  t.is(map(u8, u8).description, 'map(u8, u8)');
+  t.is(map(string, u8).description, 'map(string, u8)');
+
+  // Description can be overridden.
+  t.is(map(string, string, 'my map').description, 'my map');
+
+  // Examples with numbers.
+  const numberMap = map(u8, u8);
+  t.is(s(numberMap, new Map()), '00000000');
+  t.is(s(numberMap, new Map([[1, 2]])), '010000000102');
+  t.deepEqual(d(numberMap, '010000000102'), new Map([[1, 2]]));
+  t.deepEqual(sd(numberMap, new Map()), new Map());
+  t.deepEqual(sd(numberMap, new Map([[1, 2]])), new Map([[1, 2]]));
+  t.is(doffset(numberMap, '00000000'), 4);
+  t.is(doffset(numberMap, '010000000102'), 4 + 2);
+  t.is(doffset(numberMap, 'ff010000000102', 1), 1 + 4 + 2);
+
+  // Example with strings and numbers.
+  const letterMap = map(string, u8);
+  t.is(s(letterMap, new Map()), '00000000');
+  const letters = new Map([
+    ['a', 1],
+    ['b', 2],
+  ]);
+  t.is(
+    s(letterMap, letters),
+    '02000000' + // 2 items.
+      '0100000061' + // String 'a'.
+      '01' + // Number 1.
+      '0100000062' + // String 'b'.
+      '02' // Number 2.
+  );
+  t.deepEqual(d(letterMap, 'ff00000000', 1), new Map());
+  t.deepEqual(sd(letterMap, new Map()), new Map());
+  t.deepEqual(sd(letterMap, letters), letters);
+  t.is(doffset(letterMap, '00000000'), 4);
+});
+
 /** Serialize as a hex string. */
 function s<T, U extends T = T>(
   serializer: Serializer<T, U>,
