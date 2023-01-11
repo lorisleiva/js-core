65b7775893d9e5fc0c6289775e841f5f4ec76170
Loris Leiva <loris.leiva@gmail.com>
2023-01-11 11:10:27 +0000

    wip

--- packages/js/src/index.ts
+++ packages/js/src/index.ts
@@ -5,9 +5,10 @@ import {
 import { Web3JsRpcOptions } from '@lorisleiva/js-rpc-web3js';
 import { defaultPlugins } from './plugin';
 
-export * from './plugin';
-
 export const createMetaplex = (
   endpoint: string,
   rpcOptions?: Web3JsRpcOptions
 ): Metaplex => baseCreateMetaplex().use(defaultPlugins(endpoint, rpcOptions));
+
+export * from './plugin';
+export * from '@lorisleiva/js-core';
