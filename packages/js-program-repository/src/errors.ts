772f03b139b193aba9c1a02b6e493cb8e9ba4607
Loris Leiva <loris.leiva@gmail.com>
2023-01-25 18:10:55 +0000

    wip

--- packages/js-program-repository/src/errors.ts
+++ packages/js-program-repository/src/errors.ts
@@ -21,7 +21,7 @@ export class ProgramNotRecognizedError extends SdkError {
     const clusterString = cluster === '*' ? 'any' : `the [${cluster}]`;
     const message =
       `The provided program ${isName ? 'name' : 'address'} [${toString}] ` +
-      `is not recognized in ${clusterString} cluster.` +
+      `is not recognized in ${clusterString} cluster. ` +
       'Did you forget to register this program? ' +
       'If so, you may use "context.programs.add(myProgram)" to fix this.';
     super(message);
