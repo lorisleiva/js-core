6f20d84a47fa1f4b94d852a6631d3f77e1b19cb7
Loris Leiva <loris.leiva@gmail.com>
2023-02-02 16:09:08 +0000

    wip

--- packages/js-http-fetch/test/FetchHttp.test.ts
+++ packages/js-http-fetch/test/FetchHttp.test.ts
@@ -2,15 +2,15 @@ import test from 'ava';
 import { request } from '@lorisleiva/js-core';
 import { fetchHttp, FetchHttp } from '../src';
 
+const BASE_URL = 'http://localhost:3003/api';
+
 test('example test', async (t) => {
   t.is(typeof fetchHttp, 'function');
 });
 
 test.only('example test 2', async (t) => {
   const http = new FetchHttp();
-  const response = await http.send(
-    request().get('https://jsonplaceholder.typicode.com/todos/1').asJson()
-  );
+  const response = await http.send(request().get(`${BASE_URL}/users`).asJson());
   console.log(response);
   t.pass();
 });
