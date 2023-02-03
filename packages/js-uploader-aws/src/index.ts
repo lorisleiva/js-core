096be264ba23bfdf4e012b9bb1f4da4cc3bfcaa3
Loris Leiva <loris.leiva@gmail.com>
2023-02-03 16:03:40 +0000

    wip

--- packages/js-uploader-aws/src/index.ts
+++ packages/js-uploader-aws/src/index.ts
@@ -1,1 +1,2 @@
+export * from './AwsUploader';
 export * from './plugin';
--- packages/js-uploader-aws/test/someFeature.test.ts
+++
@@ -1,6 +0,0 @@
-import test from 'ava';
-import { awsUploader } from '../src';
-
-test('example test', async (t) => {
-  t.is(typeof awsUploader, 'function');
-});
