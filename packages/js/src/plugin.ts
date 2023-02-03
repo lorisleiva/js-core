02fda00a630e262bc4fe62f4faddda430c48dc23
Loris Leiva <loris.leiva@gmail.com>
2023-02-03 17:03:33 +0000

    wip

--- packages/js/src/plugin.ts
+++ packages/js/src/plugin.ts
@@ -1,10 +1,12 @@
 import type { MetaplexPlugin } from '@lorisleiva/js-core';
+import { httpDownloader } from '@lorisleiva/js-downloader-http';
 import { web3JsEddsa } from '@lorisleiva/js-eddsa-web3js';
 import { fetchHttp } from '@lorisleiva/js-http-fetch';
+import { defaultProgramRepository } from '@lorisleiva/js-program-repository';
 import { web3JsRpc, Web3JsRpcOptions } from '@lorisleiva/js-rpc-web3js';
 import { beetSerializer } from '@lorisleiva/js-serializer-beet';
-import { defaultProgramRepository } from '@lorisleiva/js-program-repository';
 import { web3JsTransactionFactory } from '@lorisleiva/js-transaction-factory-web3js';
+import { nftStorageUploader } from '@lorisleiva/js-uploader-nft-storage';
 
 export const defaultPlugins = (
   endpoint: string,
@@ -14,6 +16,8 @@ export const defaultPlugins = (
     metaplex.use(beetSerializer());
     metaplex.use(defaultProgramRepository());
     metaplex.use(fetchHttp());
+    metaplex.use(httpDownloader());
+    metaplex.use(nftStorageUploader());
     metaplex.use(web3JsEddsa());
     metaplex.use(web3JsRpc(endpoint, rpcOptions));
     metaplex.use(web3JsTransactionFactory());
